ea3fcb3c3254a48e56cea7ea6cfeb9b686db9f19
komok113 <abdulhakim.dev@gmail.com>
2019-10-03 19:32:31 +0300

    First Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 25);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(475, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button
+            // 
+            this.Button.Location = new System.Drawing.Point(176, 303);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(106, 35);
+            this.Button.TabIndex = 1;
+            this.Button.Text = "Button";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(476, 171);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 350);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(741, 116);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Программа";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int move = 0, moveX = 0, moveY = 0;
+            panel1.MouseDown += (s, e) => { move = 1; moveX = e.X; moveY = e.Y; };
+            panel1.MouseMove += (s, e) => { if (move == 1) SetDesktopLocation(MousePosition.X - moveX, MousePosition.Y - moveY); };
+            panel1.MouseUp += (s, e) => { move = 0; };
+        }
+
+        void Button1_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+
+        async void Button_Click(object sender, System.EventArgs e)
+        {
+            // начальный цвет 70,130,180
+            //желаемый цвет 135,206,250
+
+            //for (byte r = 70, g = 130, b = 180; r <= 135 & g <= 206 & b <= 250; r += 6, g += 7, b += 7, await Task.Delay(100))
+            //{
+            //    label1.ForeColor = Color.FromArgb(r, g, b);
+            //}
+
+            //await Task.Delay(2000);
+
+            //for(byte r = 135, g = 206, b = 250; r >= 70 & g >= 130 & b >= 180; r-=6, g-=7, b -= 7, await Task.Delay(100))
+            //{
+            //    label1.ForeColor = Color.FromArgb(r, g, b);
+            //}
+
+            //label1.ForeColor = Color.FromArgb(70, 130, 180);
+
+            char[] chars = "Джамиля улыбнись! Посмотри сколько людей тебя любят! Не время грустить, время веселиться!".ToCharArray();
+            foreach(char ch in chars)
+            {
+                await Task.Delay(80);
+                label1.Text += ch.ToString();
+            }
+        }
+    }
+}
